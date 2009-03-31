19dfe0118f748662e43c9b7cd6429e88397be543
Dann <techmemo@gmail.com>
2009-03-31 09:42:56 +0900

    force build

--- lib/CPAN/Packager/Builder/RPM.pm
+++ lib/CPAN/Packager/Builder/RPM.pm
@@ -195,14 +195,9 @@ sub build_rpm_package {
         = "--rcfile $rpmrc_file -ba --rmsource --rmspec --clean $spec_file_path";
     $build_opt = "--rcfile $rpmrc_file -ba $spec_file_path"
         if &CPAN::Packager::DEBUG;
-    my $retval
-        = system("env PERL_MM_USE_DEFAULT=1 LANG=C rpmbuild $build_opt");
-
-    # FIXME fix later
-    $retval = $? >> 8;
-    if ( $retval != 0 ) {
-        Carp::croak "RPM building failed!\n";
-    }
+    my $result = capture(EXIT_ANY, "env PERL_MM_USE_DEFAULT=1 LANG=C rpmbuild $build_opt");
+    warn $result if &CPAN::Packager::DEBUG;
+    $result;
 }
 
 sub copy_module_sources_to_build_dir {
