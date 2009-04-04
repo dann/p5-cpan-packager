ee30b404ef48412607c4bb4404067c85cc336a60
Dann <techmemo@gmail.com>
2009-04-04 18:21:16 +0900

    added debug messages

--- lib/CPAN/Packager.pm
+++ lib/CPAN/Packager.pm
@@ -57,23 +57,30 @@ has 'always_build' => (
 sub make {
     my ( $self, $module, $built_modules ) = @_;
     die 'module must be passed' unless $module;
+    $self->log( info => "### Building packages for $module ... ###" );
     my $config = $self->config_loader->load( $self->conf );
     $config->{modules} = $built_modules if $built_modules;
 
     my $modules = $self->analyze_module_dependencies( $module, $config );
     $config = $self->merge_config( $modules, $config )
         if $self->conf;
-    $self->_dump_modules( $config->{modules} );
-    $built_modules = $self->build_modules( $config->{modules}, $config );
+    eval {
+        $built_modules = $self->build_modules( $config->{modules}, $config );
+    };
+    if ($@) {
+        $self->_dump_modules( $config->{modules} );
+        die "### Built packages for $module faied :-( ###" . $@;
+    }
+    $self->log( info => "### Builtpackages for $module faied :-( ### " );
     $built_modules;
 }
 
 sub _dump_modules {
     my ( $self, $modules ) = @_;
-    if (DEBUG) {
+    #if (DEBUG) {
         require Data::Dumper;
-        $self->log( debug => Data::Dumper::Dumper $modules );
-    }
+        $self->log( debug =>"Modules info \n" . Data::Dumper::Dumper $modules );
+        #}
 }
 
 sub merge_config {
@@ -84,7 +91,8 @@ sub merge_config {
 sub build_modules {
     my ( $self, $modules, $config ) = @_;
     my $builder_name = $self->builder;
-    $self->log( info => "making packages for $builder_name ..." );
+    $self->log(
+        info => "Building ${builder} packages for $builder_name ..." );
 
     my $builder
         = CPAN::Packager::BuilderFactory->create( $builder_name, $config );
