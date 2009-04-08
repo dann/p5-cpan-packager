6d1e9109d89bd00cb7c4e4f2e9ab2134217a9a55
Dann <techmemo@gmail.com>
2009-04-09 08:23:27 +0900

    fixed POD bug

--- lib/CPAN/Packager.pm
+++ lib/CPAN/Packager.pm
@@ -7,7 +7,7 @@ use CPAN::Packager::DependencyConfigMerger;
 use CPAN::Packager::ConfigLoader;
 with 'CPAN::Packager::Role::Logger';
 
-our $VERSION = '0.04';
+our $VERSION = '0.041';
 
 BEGIN {
     if ( !defined &DEBUG ) {
