f63dbc980072d0b5524f9bad615d00e39ba44c84
Dann <techmemo@gmail.com>
2009-06-19 23:10:28 +0900

    added doc

--- tools/validator.pl
+++ tools/validator.pl
@@ -3,5 +3,40 @@ use FindBin::libs;
 use CPAN::Packager::ConfigLoader;
 use CPAN::Packager::Config::Validator;
 
-my $config = CPAN::Packager::ConfigLoader->load($ARGV[0]);
-CPAN::Packager::Config::Validator->validate($config);
+main();
+exit;
+
+sub main {
+    my $config_path = $ARGV[0];
+    die 'config is needed' unless $config_path;
+    validate_config($config_path);
+}
+
+sub validate_config {
+    my $config_path = shift;
+    my $config = CPAN::Packager::ConfigLoader->load($config_path);
+    CPAN::Packager::Config::Validator->validate($config);
+}
+
+=head1 NAME
+
+validator  - valdiate config 
+
+=head1 SYNOPSIS
+
+  validator conf/config-rpm.yaml 
+
+=head1 DESCRIPTION
+
+=head1 AUTHOR
+
+Takatoshi Kitano E<lt>kitano.tk@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
