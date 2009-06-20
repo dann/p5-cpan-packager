05b3222a81f9d753004b4151ff376bed7e79b764
Dann <techmemo@gmail.com>
2009-06-21 08:45:17 +0900

    updated version

--- lib/CPAN/Packager.pm
+++ lib/CPAN/Packager.pm
@@ -9,7 +9,7 @@ use CPAN::Packager::Config::Loader;
 use CPAN::Packager::Util;
 with 'CPAN::Packager::Role::Logger';
 
-our $VERSION = '0.052';
+our $VERSION = '0.053';
 
 BEGIN {
     if ( !defined &DEBUG ) {
