2e8acaa10d6f0d70bc40d8dd21e361334fe37596
Dann <techmemo@gmail.com>
2009-07-06 12:50:42 +0900

    bumb version

--- lib/CPAN/Packager.pm
+++ lib/CPAN/Packager.pm
@@ -9,7 +9,7 @@ use CPAN::Packager::Config::Loader;
 use CPAN::Packager::Util;
 with 'CPAN::Packager::Role::Logger';
 
-our $VERSION = '0.053';
+our $VERSION = '0.054';
 
 BEGIN {
     if ( !defined &DEBUG ) {
