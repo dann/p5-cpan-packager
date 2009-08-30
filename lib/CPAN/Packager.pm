db083ab28f1c3d202fef7b90c9c5a2430e34a7d9
Dann <techmemo@gmail.com>
2009-08-31 02:10:43 +0900

    Checking in changes prior to tagging of version 0.073.  Changelog diff is:

--- lib/CPAN/Packager.pm
+++ lib/CPAN/Packager.pm
@@ -9,7 +9,7 @@ use CPAN::Packager::Config::Loader;
 use CPAN::Packager::Util;
 with 'CPAN::Packager::Role::Logger';
 
-our $VERSION = '0.072';
+our $VERSION = '0.073';
 
 BEGIN {
     if ( !defined &DEBUG ) {
