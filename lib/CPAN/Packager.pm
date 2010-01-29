88a71221b5f398f1b264c9591261a73bc9dcb048
Dann <techmemo@gmail.com>
2010-01-29 20:32:30 +0900

    depends Try::Tiny

--- lib/CPAN/Packager.pm
+++ lib/CPAN/Packager.pm
@@ -10,7 +10,7 @@ use CPAN::Packager::Config::Loader;
 use CPAN::Packager::Util;
 use Log::Log4perl qw(:easy);
 
-our $VERSION = '0.09';
+our $VERSION = '0.1';
 
 has 'builder' => (
     is      => 'rw',
