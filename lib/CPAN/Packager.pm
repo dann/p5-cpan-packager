bf658f76afe592cc3866c61d845d78e8a9b652c6
Dann <techmemo@gmail.com>
2010-01-31 12:47:02 +0900

    move logger settings to CPAN::Packager.

--- lib/CPAN/Packager.pm
+++ lib/CPAN/Packager.pm
@@ -68,16 +68,26 @@ has 'verbose' => (
 sub BUILD {
     my $self = shift;
     $self->_setup_dependencies();
-    $self->_enable_debug if $self->verbose;
-}
-
-sub _enable_debug {
-    get_logger->level($DEBUG);
+    $self->_setup_logger();
 }
 
 sub _setup_dependencies {
     my $self = shift;
     $self->_build_dependency_analyzer;
+}
+
+sub _setup_logger {
+    my $self = shift;
+    my $level = $self->verbose ? $DEBUG : $INFO;
+    my $layout = '%p: %m{chomp}%n';
+
+    if($ENV{CPAN_PACKAGER_DEBUG}) {
+        $level = $DEBUG; 
+        $layout = '%p %d{HH:mm:ss} [%c:%L]: %m{chomp}%n';
+    }
+
+    Log::Log4perl->easy_init({ level => $level,
+                           layout => $layout });
 }
 
 sub _build_dependency_analyzer {
@@ -235,19 +245,21 @@ anyone is missing from this list.
 
 =head1 For Developers
 
+=head2 Use CPAN_PACKAGER_DEBUG environment to debug building a distribution package
+
+Debug messages are displayed when you use the verbose option of the
+cpan-packager script.
+
+  CPAN_PACKAGER_DEBUG=1 bin/cpan-packager --conf conf/config-rpm.yaml --module Acme::Bleach 
+    --builder RPM
+
 =head2 How to do live tests
 
 Set the CPAN_PACKAGER_TEST_LIVE environment variable when you execute prove:
 
   CPAN_PACKAGER_TEST_LIVE=1 prove -lv t/it/010_build_rpm/*.t
 
-=head2 Use verbose option to debug building a distribution package
-
-Debug messages are displayed when you use the verbose option of the
-cpan-packager script.
-
-  bin/cpan-packager --conf conf/config-rpm.yaml --module Acme::Bleach 
-    --builder RPM --verbose
+=head2 Use 
 
 =head1 LICENSE
 
